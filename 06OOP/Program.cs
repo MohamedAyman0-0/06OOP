c7aa290c2a61a325eb3e2b4d830d511a01541c43
mohamed ayman <mohamedaymanabdel3ty@gmail.com>
2025-08-19 20:51:15 +0300

    1.

--- 06OOP/Program.cs
+++ 06OOP/Program.cs
@@ -1,1 +1,36 @@
-﻿
+﻿#region first project
+#region 1.
+public class Point3D
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Z { get; set; }
+
+    // Constructor 1: Default
+    public Point3D() : this(0, 0, 0) { }
+
+    // Constructor 2: X only
+    public Point3D(int x) : this(x, 0, 0) { }
+
+    // Constructor 3: X & Y
+    public Point3D(int x, int y) : this(x, y, 0) { }
+
+    // Constructor 4: X, Y & Z (Main constructor)
+    public Point3D(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
+
+#endregion
+#region 2.
+#endregion
+#region
+#endregion
+#region
+#endregion
+#region
+#endregion
+#endregion
